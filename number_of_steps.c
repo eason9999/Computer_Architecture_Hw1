58bfa9a6091ea0ed01f4b070e074fd1401b65fc5
Kai Liao <esaon9784000@gmail.com>
2024-10-10 00:53:53 +0800

    Add Number_of_Steps.c

--- number_of_steps.c
+++ number_of_steps.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
-
-int numberOfSteps(int num) {
-    int steps = 0;
-    while (num > 0) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num -= 1;
-        }
-        steps++;
+#include <stdint.h>
+int length(uint32_t num) {
+    uint32_t clz = 0;
+    if ((num >> 16) == 0) {
+        clz += 16;
+        num <<= 16;
     }
-    return steps;
+    if ((num >> 24) == 0) {
+        clz += 8;
+        num <<= 8;
+    }
+    if ((num >> 28) == 0) {
+        clz += 4;
+        num <<= 4;
+    }
+    if ((num >> 30) == 0) {
+        clz += 2;
+        num <<= 2;
+    }
+    if ((num >> 31) == 0) {
+        clz += 1;
+    }
+    return 31 - clz;
 }
 
+uint32_t my_popcnt (uint32_t u)
+ {
+      u = (u & 0x55555555) + ((u >> 1) & 0x55555555);
+      u = (u & 0x33333333) + ((u >> 2) & 0x33333333);
+      u = (u & 0x0F0F0F0F) + ((u >> 4) & 0x0F0F0F0F);
+      u = (u & 0x00FF00FF) + ((u >> 8) & 0x00FF00FF);
+      u = (u & 0x0000FFFF) + ((u >> 16) & 0x0000FFFF);
+      return u;
+ }
 int main() {
     int test_data[] = {14, 8, 123};
-    int expected_results[] = {6, 4, 12};
     
     for (int i = 0; i < 3; i++) {
-        int result = numberOfSteps(test_data[i]);
-        if (result == expected_results[i]) {
-            printf("Test %d passed. Result: %d\n", i + 1, result);
-        } else {
-            printf("Test %d failed. Expected: %d, Got: %d\n", i + 1, expected_results[i], result);
-        }
+        int result = test_data[i] ? my_popcnt(test_data[i]) + length(test_data[i]) : 0;
+        printf("Input num = %d Output = %d\n",test_data[i],result);
     }
 
     return 0;
-}
+}   
